cd85a2fbba8d90c331c9f5daf4980ac86cf5d391
Serius17 <92595056+Serius17@users.noreply.github.com>
2022-12-30 13:35:44 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+@extends('layout.master')
+@section('title', 'Home')
+@section('content')
+    <h1 class="cover-heading">Hello!
+    </h1>
+    <p class="lead">
+        This Is FLYON-AR
+    </p>
+    <p class="text-left">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis molestias exercitationem optio, at, ipsum,
+        eius blanditiis mollitia saepe nihil quae esse consequuntur atque nostrum assumenda quisquam quidem! Modi, obcaecati
+        minima?
+    </p>
+@endsection
